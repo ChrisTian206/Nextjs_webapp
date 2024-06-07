1175e29590ee5ba7a169cefdefbc11e4bbce426a
Wenhao Tian <wtian16@bu.edu>
2024-06-07 15:18:38 -0700

    added submit function

--- ticketingapp/components/TicketForm.tsx
+++ ticketingapp/components/TicketForm.tsx
@@ -1,31 +1,62 @@
-"use client"
+"use client" // client side render, because it requires user interactions.
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, FormControl, FormLabel, FormField, FormItem } from './ui/form'
 import { ticketSchema } from '@/ValidationSchemas/ticket'
 import { z } from 'zod'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Input } from './ui/input'
-
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
+import { Button } from './ui/button'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
+//Using Shadcn UI, make sure to import the Comps from ui/<Comp>, NOT FROM redix
 
 type TicketFormData = z.infer<typeof ticketSchema>
 
 const TicketForm = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [error, setError] = useState("")
+    const router = useRouter() //make sure to use the one from next/navigation
 
     const form = useForm<TicketFormData>({
         resolver: zodResolver(ticketSchema)
     })
 
     const onSubmit = async (values: z.infer<typeof ticketSchema>) => {
-        console.log(values)
+        try {
+            setIsSubmitting(true)
+            setError("")
+
+            await axios.post("/api/tickets", values)
+            //ps. if values is {values}, it failed zodValidation. It would say invalid type for every field.
+
+            setIsSubmitting(false)
+            //always a good practice to reset it back
+            //cases are when user has slow speed, they might be confused.
+
+            router.push("/tickets")
+            router.refresh()
+            //Here is why this refresh() is needed:
+            /**
+             * Client-side Navigation: When using useRouter from next/navigation to navigate back to the Table page, Next.js performs a client-side navigation, which means it doesn't automatically trigger a full page reload or a re-fetch of data unless configured to do so.
+             * Caching: Depending on how the data fetching is set up, the data might be cached and not automatically updated on client-side navigation.
+             * Lack of Data Re-fetch Trigger: If your data fetching logic doesn't have a mechanism to re-fetch the data upon navigation, the Table page will display the old data it initially fetched.
+             */
+        } catch (error) {
+            console.log("error onSubmit: ", error)
+            setError("Error Occured, please try again later.")
+            setIsSubmitting(false)
+        }
     }
 
     return (
         <div className='rounded-md border w-full p-4'>
+            {error && <h1 className='text-red-500 text-center'>{error}</h1>}
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 w-full'>
                     <FormField
@@ -95,6 +126,8 @@ const TicketForm = () => {
                                 </FormItem>
                             )} />
                     </div>
+
+                    <Button type='submit' disabled={isSubmitting}>Submit</Button>
                 </form>
             </Form>
         </div>
