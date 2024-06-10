00073b2062a2e037a6e801389e06b1e2c3aefcca
Wenhao Tian <wtian16@bu.edu>
2024-06-10 14:03:33 -0700

    It now can take ticket as props. Can be used to new/update tickets.

--- ticketingapp/components/TicketForm.tsx
+++ ticketingapp/components/TicketForm.tsx
@@ -13,12 +13,17 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '.
 import { Button } from './ui/button'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { Ticket } from '@prisma/client'
 
 //Using Shadcn UI, make sure to import the Comps from ui/<Comp>, NOT FROM redix
 
 type TicketFormData = z.infer<typeof ticketSchema>
 
-const TicketForm = () => {
+interface Props {
+    ticket?: Ticket
+}
+
+const TicketForm = ({ ticket }: Props) => {
     const [isSubmitting, setIsSubmitting] = useState(false)
     const [error, setError] = useState("")
     const router = useRouter() //make sure to use the one from next/navigation
@@ -32,7 +37,11 @@ const TicketForm = () => {
             setIsSubmitting(true)
             setError("")
 
-            await axios.post("/api/tickets", values)
+            if (ticket) {
+                await axios.patch("/api/tickets/" + ticket.id, values)
+            } else {
+                await axios.post("/api/tickets/", values)
+            }
             //ps. if values is {values}, it failed zodValidation. It would say invalid type for every field.
 
             setIsSubmitting(false)
@@ -62,6 +71,7 @@ const TicketForm = () => {
                     <FormField
                         control={form.control}
                         name="title"
+                        defaultValue={ticket?.title}
                         render={({ field }) => (
                             <FormItem>
                                 <FormLabel>Ticket Title</FormLabel>
@@ -75,6 +85,7 @@ const TicketForm = () => {
                     <Controller
                         name='description'
                         control={form.control}
+                        defaultValue={ticket?.description}
                         render={({ field }) => (
                             <SimpleMDE placeholder='description' {...field} />
                         )} />
@@ -83,6 +94,7 @@ const TicketForm = () => {
                         <FormField
                             control={form.control}
                             name="status"
+                            defaultValue={ticket?.status}
                             render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Status</FormLabel>
@@ -107,6 +119,7 @@ const TicketForm = () => {
                         <FormField
                             control={form.control}
                             name="priority"
+                            defaultValue={ticket?.priority}
                             render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Priority</FormLabel>
@@ -127,7 +140,11 @@ const TicketForm = () => {
                             )} />
                     </div>
 
-                    <Button type='submit' disabled={isSubmitting}>Submit</Button>
+                    <Button type='submit' disabled={isSubmitting}>
+                        {
+                            ticket ? "Update Ticket" : "Submit Ticket"
+                        }
+                    </Button>
                 </form>
             </Form>
         </div>
