1574fcf3ca276b3c68989feaa848d6e5b24a3415
Wenhao Tian <wtian16@bu.edu>
2024-06-12 14:37:15 -0700

    Added Delete API

--- ticketingapp/app/api/tickets/[id]/route.ts
+++ ticketingapp/app/api/tickets/[id]/route.ts
@@ -35,3 +35,22 @@ export async function PATCH(request: NextRequest, { params }: Props) {
 
     return NextResponse.json(updateTicket)
 }
+
+export async function DELETE(request: NextRequest, { params }: Props) {
+
+    const ticket = await prisma.ticket.findUnique({
+        where: { id: parseInt(params.id) },
+    })
+
+    if (!ticket) {
+        return NextResponse.json({ error: "Ticket Not Found" }, { status: 404 })
+    }
+
+    await prisma.ticket.delete({
+        where: { id: ticket.id },
+    }).catch((err) => {
+        return NextResponse.json({ message: "Failed to delete", error: err }, { status: 500 })
+    })
+
+    return NextResponse.json({ message: "Ticket is deleted" })
+}
