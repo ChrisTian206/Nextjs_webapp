c42049c866bae2c393dc18c5b3eb360a9c269792
Wenhao Tian <wtian16@bu.edu>
2024-06-21 12:01:11 -0700

    added add+ user api

---
+++ ticketingapp/app/api/users/route.ts
@@ -0,0 +1,32 @@
+import { userSchema } from "@/ValidationSchemas/users";
+import prisma from "@/prisma/db";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcryptjs'
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const validation = userSchema.safeParse(body)
+
+    if (!validation.success) {
+        return NextResponse.json(validation.error.format(), { status: 400 })
+    }
+
+    const isUserAlreadyExist = await prisma.user.findUnique({
+        where: {
+            id: body.id
+        }
+    })
+
+    if (isUserAlreadyExist) {
+        return NextResponse.json({ message: "User already existed." }, { status: 409 })
+    }
+
+    const hashPw = await bcrypt.hash(body.password, 3)
+    body.password = hashPw
+
+    const newUser = await prisma.user.create({
+        data: { ...body }
+    })
+
+    return NextResponse.json(newUser, { status: 201 })
+}
